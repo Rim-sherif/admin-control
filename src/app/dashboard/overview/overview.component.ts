b7e1f6c5cb43b355c35f719b5684a7c56192f693
Mohamed Hamed <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2025-04-15 03:55:51 +0200

    Update overview.component.ts

--- src/app/dashboard/overview/overview.component.ts
+++ src/app/dashboard/overview/overview.component.ts
@@ -1,5 +1,6 @@
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard.service';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {
   faChartLine,
@@ -13,18 +14,42 @@ import {
   standalone: true,
   imports: [CommonModule, FontAwesomeModule],
   templateUrl: './overview.component.html',
+  styleUrls: ['./overview.component.css'],
 })
-export class OverviewComponent {
- 
+export class OverviewComponent implements OnInit {
   faChartLine = faChartLine;
   faUsers = faUsers;
   faGraduationCap = faGraduationCap;
   faMoneyBill = faMoneyBill;
 
-  stats = {
-    totalStudents: 1234,
-    totalCourses: 45,
-    totalRevenue: 54321,
-    completionRate: 78
+  dashboardData: any = {
+    approvedCourse: 0,
+    pendingCourse: 0,
+    rejectedCourse: 0,
+    instructorCount: 0,
+    userCount: 0,
+    adminCount: 0,
+    totalEnrollments: 0,
+    totalAdminEarnings: 0,
+    totalInstructorEarnings: 0,
   };
-} 
+
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.loadDashboardData();
+  }
+
+  loadDashboardData(): void {
+    this.dashboardService.getAdminSummary().subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.dashboardData = response.data;
+        }
+      },
+      error: (error) => {
+        console.error('Error loading dashboard data:', error);
+      },
+    });
+  }
+}
